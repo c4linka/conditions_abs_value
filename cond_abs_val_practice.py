1a4db254d9d5b37c953823d5d9054cc8bcf45ab0
Ewa <155647516+c4linka@users.noreply.github.com>
2024-01-08 09:44:47 +0100

    Add files via upload
    Just practicing.

---
+++ cond_abs_val_practice.py
@@ -0,0 +1,10 @@
+number = int(input("Write the number to check it's the absolute value "))
+numberAbs = abs(number)
+print("The absolute value of",(number),"is",(numberAbs),":)")
+
+number = int(input("Write the number to check it's absolute value, but in different way ;) "))
+
+if number < 0:
+    number = -number
+    
+print("The absolute value is",(number))
